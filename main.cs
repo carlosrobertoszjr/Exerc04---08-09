b933834ca5217915e1d02b10c2d615dd4b0fdb4d
Carlos Roberto <carlosrobertoszjr@gmail.com>
2020-09-13 00:41:11 +0000

    Concluído.

--- main.cs
+++ main.cs
@@ -5,8 +5,10 @@ class MainClass {
     Pessoa Carlos = new Pessoa();
     //COLOQUE SUA IDADE DENTRO DOS PARENTERESES
     Console.WriteLine(Carlos.Envelhecer(20));
+
     //COLOQUEI ALTURA E IDADE DENTRO DOS PARENTERESES
     Console.WriteLine(Carlos.Crescer(1.85,22));
+    
     //COLOQUE ALTURA E PESO DENTRO DOS PARENTES
     Console.WriteLine(Carlos.Emagrecer(1.85,90));
     Console.WriteLine(Carlos.Engordar(1.85,90));
