5ccf9ba2d4555673b5d423df1dcc35557c1bdeff
Carlos Roberto <carlosrobertoszjr@gmail.com>
2020-09-13 00:42:28 +0000

    Concluído.

--- main.cs
+++ main.cs
@@ -7,8 +7,8 @@ class MainClass {
     Console.WriteLine(Carlos.Envelhecer(20));
 
     //COLOQUEI ALTURA E IDADE DENTRO DOS PARENTERESES
-    Console.WriteLine(Carlos.Crescer(1.85,22));
-    
+    Console.WriteLine(Carlos.Crescer(1.85,20));
+
     //COLOQUE ALTURA E PESO DENTRO DOS PARENTES
     Console.WriteLine(Carlos.Emagrecer(1.85,90));
     Console.WriteLine(Carlos.Engordar(1.85,90));
